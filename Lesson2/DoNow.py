4ddefcd1ed710136ea9378cd525c772d210d87af
JoseNolasco08 <dannyou007@gmail.com>
2025-06-04 17:38:38 -0500

    Update DoNow.py

---
+++ Lesson2/DoNow.py
@@ -0,0 +1,33 @@
+#You want the function below to print a greeting message to the screen.
+# This code will not be executed until it is 'called'.
+def say_hello():
+  print('Hello!')  #change Hello to your favorite greeting
+
+
+# Our function is defined above, so now we can 'call' it
+say_hello()
+
+
+
+
+
+
+#Ex 1
+
+sandals_count = 3
+sneakers_count = 1
+boots_count = 2
+
+
+
+total_shoes = # Write code here
+
+
+
+
+#Ex 2
+
+quarters_per_dollar =4
+number_of_dollars =5
+
+number_of_quarters = number_of_dollars - quarters_per_dollar
